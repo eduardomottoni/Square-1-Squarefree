a382c991130085daefd283a765b4e9c89a2f5bdf
Eduardo Ottoni <HCC-EOtoni@MBPLC.com>
2023-11-22 15:23:45 +0100

    working function

--- Square+1=Squarefree.py
+++ Square+1=Squarefree.py
@@ -1,36 +1,4 @@
-from sympy import primerange
-from math import isqrt
-
-def generate_primes_up_to_limit(limit):
-    # Generate a set of prime numbers up to the given limit
-    return set(primerange(2, limit))
-
-def is_squarefree(n, primes):
-    # Check if n is squarefree
-
-    # If n is even, divide it by 2 using bitwise operations
-    if n & 1 == 0:
-        n >>= 1
-
-    # If 2 again divides n, then n is not a squarefree number.
-    if n & 1 == 0:
-        return False
-
-    # Calculate the square root of n only once
-    sqrt_n = isqrt(n)
-
-    # Iterate through the precomputed set of primes
-    for prime in primes:
-        # If the prime is greater than the square root of n, break the loop
-        if prime > sqrt_n:
-            break
-        # If n is divisible by the square of the current prime, it's not squarefree
-        if n % (prime**2) == 0:
-            return False
-    
-    # Return True if n is squarefree
-    return True
-def square_free_sieve(limit,squarefree_count):
+def square_free_sieve(limit):
     """Generator that yields all square free numbers less than limit"""
     a = [True] * limit
     # Needed so we don't mark off multiples of 1^2
@@ -39,37 +7,22 @@ def square_free_sieve(limit,squarefree_count):
     for i, is_square_free in enumerate(a):
         if is_square_free:
             yield i
-            
-            i2 = i * i +1
+            i2 = i * i
             for n in range(i2, limit, i2):
                 a[n] = False
-            
-# def count_squarefree_numbers(limit):
-#     squarefree_count = 0
 
-#     # Generate a set of primes up to the square root of the limit
-#     primes_up_to_sqrt_limit = generate_primes_up_to_limit(isqrt(limit))
+# Defina o limite como um número inteiro
+# Defina o limite como um número inteiro
+limit_int = 123567101113
 
-#     # Iterate through numbers up to the given limit
-#     for p in range(1, limit + 1):
-#         # Calculate the candidate number
-#         candidate = p**2 + 1
-        
-#         # Check if the candidate is squarefree using the precomputed set of primes
-#         if is_squarefree(candidate, primes_up_to_sqrt_limit):
-#             squarefree_count += 1
+# Crie o conjunto de números sem quadrados perfeitos
+squares_set = {n**2 + 1 for n in range(1, limit_int + 1)}
 
-#     return squarefree_count
+# Use a função square_free_sieve com o limite
+test2_set = set(square_free_sieve(max(squares_set)))
 
-def count_squarefree_numbers(limit):
-    squarefree_count = 0
+# Encontre a interseção entre os conjuntos
+result_set = squares_set.intersection(test2_set)
 
-    # Generate a set of primes up to the square root of the limit
-   
-    return len(list(square_free_sieve(limit,squarefree_count)))
-# Example usage
-
-#limit = 123567101113
-limit = 100
-result = count_squarefree_numbers(limit)
-print(result)
+print(result_set)
+print(len(result_set))
